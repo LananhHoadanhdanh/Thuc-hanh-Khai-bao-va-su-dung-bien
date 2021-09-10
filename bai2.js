bc036d0c12402a62d5b01fc814e8c6682fd694c8
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2021-09-10 10:28:47 +0700

    xong

---
+++ bai2.js
@@ -0,0 +1,4 @@
+let width = 20;
+let height = 10;
+let aria = width * height;
+document.write("aria="+aria)
---
+++ bai3.js
@@ -0,0 +1,11 @@
+let a;
+let b;
+a = prompt("Nhập vào a");
+b = prompt("Nhập vào b");
+let sodu = a % b;
+if(sodu==0) {
+    alert("a là bội số của b");
+}
+else {
+    alert("a không là bội số của b");
+}
